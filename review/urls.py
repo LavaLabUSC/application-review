24760489aa800da7e7e5c56032059283fe624570
drakerehfeld <drakerehfeld@gmail.com>
2016-08-28 20:09:44 -0700

    redir index to review

--- review/urls.py
+++ review/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic.base import RedirectView
 
 # from . import views
 
 urlpatterns = [
+	url(r'^$', RedirectView.as_view(url='/review', permanent=True), name='index'),
     url(r'^admin/', admin.site.urls),
     url(r'^review/', include('webview.urls')),
 ]
